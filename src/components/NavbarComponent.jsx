cb684c211372dfde6efd21b86f30228d182ea983
Mom Rotha <rothamom22@gmail.com>
2025-02-17 19:47:57 +0700

    modified

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -6,7 +6,7 @@ import { Navbar } from "flowbite-react";
 export function NavbarComponent() {
   return (
     <Navbar fluid rounded className="bg-pink-200 fixed top-0 left-0 w-full z-50">
-      <Navbar.Brand  href="/assets/react.svg">
+      <Navbar.Brand  href="/">
         <img src="logo.png" className="rounded-full h-16 w-16 border-2 border-blue-500 bg-white p-1" alt="Flowbite React Logo" />
         <img src="namelogo.png" className=" h-16 w-32" alt="Flowbite React Logoname" />
         {/* <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span> */}

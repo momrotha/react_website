8459bbc0d9fc96374ac9905938e4278a16d9ccb0
Mom Rotha <rothamom22@gmail.com>
2025-02-17 18:28:53 +0700

    push again1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/", // Set base path for Vercel
   plugins: [react()],
+  base: "./", // Fix for Vercel Deployment
 });
-
